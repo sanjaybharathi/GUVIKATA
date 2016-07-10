9ca91f08d8515639c7af6aefae3c169f79a89d2b
sanjaybharathi <sanjaybharathi95@gmail.com>
2016-07-10 15:31:31 +0530

    Create Odd_range.java

---
+++ Odd_range.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+public class Odd_range
+{
+public static void main(String args[])
+{
+int t1,t2,i=0;
+System.out.println("Enter the intervals:");
+Scanner s=new Scanner(System.in);
+t1=s.nextInt();
+t2=s.nextInt();
+for(i=t1;i<=t2;i++)
+{
+if(i%2==0)
+System.out.println(" ");
+else
+System.out.println(+i);
+}
+}
+}
