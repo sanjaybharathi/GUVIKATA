48e89b31a992e5983df77f887600be90b4d3748c
sanjaybharathi <sanjaybharathi95@gmail.com>
2016-07-10 11:42:11 +0530

    Create tower.c

---
+++ tower.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void tow(int,char,char,char);
+void main()
+{
+int n;
+scanf("%d",&n);
+tower(n,'X','Y','Z');
+}
+void tow(int n,char src,char tm,char dest)
+{
+if(n==1)
+{
+printf("\n Move disk 1 from src %c to dest %c",src,tm);
+return;
+}
+tow(n-1,src,dest,tm);
+printf("\n Move disk %d from src %c to dest %c",n,src,tm);
+tow(num - 1,dest,tm,src);
+}
