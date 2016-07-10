6c51185cbef856c478b40fac9690621b9138d0ab
sanjaybharathi <sanjaybharathi95@gmail.com>
2016-07-10 15:00:14 +0530

    Create alpha.c

---
+++ alpha.c
@@ -0,0 +1,16 @@
+import java.io.*;
+	import java.util.*;
+	public class Vowel
+	{
+	public static void main(String args[])
+	{
+	String str;
+	System.out.println("Enter the character:");
+	Scanner s=new Scanner(System.in);
+	str=s.next();
+	if(str.equals("a")||str.equals("e")||str.equals("i")||str.equals("o")||str.equals("u"))
+	System.out.println("Vowel");
+	else
+	System.out.println("Constant");
+	}
+	}
