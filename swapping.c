812d934c3f45761dfb3b2dffdbf6dd72ecaea9b9
sanjaybharathi <sanjaybharathi95@gmail.com>
2016-07-10 15:41:11 +0530

    Update swapping.c

--- swapping.c
+++ swapping.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main(void) {
-	int a,b,temp=0;
+	int a,b,t=0;
   printf("\n Enter the integer value for swapping a and b:");
   scanf("%d%d",&a,&b);
   printf("\n value of a and b before swapping is %d and %d",a,b);
-  temp=a;
+  t=a;
   a=b;
-  b=temp;
+  b=t;
     printf("\n value of a and b after swapping is %d and %d",a,b);
  
 	return 0;
