dbbb00372cfe875fd7b7ed889df96f854a51da7a
sanjaybharathi <sanjaybharathi95@gmail.com>
2016-09-02 13:54:36 +0530

    Create Rev.java

---
+++ Rev.java
@@ -0,0 +1,25 @@
+import java.lang.*;
+import java.util.scanner;
+public class Rev
+{
+public static void main(String[] args)
+{
+Scanner sca=new Scanner(System.in);
+String strs;
+strs=sca.next();
+int k=strs.next();
+k--;
+		char st[]=strs.toCharArray();
+		char t;
+		for(int i=0;i<st.length/2;i++){
+			t=st[i];
+			st[i]=st[k];
+			st[k]=t;
+			k--;
+		}
+		strs=new String(st);
+		System.out.println(strs);
+
+	}
+
+}
