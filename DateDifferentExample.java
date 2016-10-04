3f1afc4ab0cee2c7d61310c5140478e2d6e96891
sanjaybharathi <sanjaybharathi95@gmail.com>
2016-10-04 16:23:28 +0530

    Update DateDifferentExample.java

--- DateDifferentExample.java
+++ DateDifferentExample.java
@@ -8,7 +8,6 @@ class DateDifferentExample {
 		String dateStart = "01/14/2012 09:29:58";
 		String dateStop = "01/15/2012 10:31:48";
 
-		//HH converts hour in 24 hours format (0-23), day calculation
 		SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
 
 		Date d1 = null;
@@ -18,7 +17,7 @@ class DateDifferentExample {
 			d1 = format.parse(dateStart);
 			d2 = format.parse(dateStop);
 
-			//in milliseconds
+			
 			long diff = d2.getTime() - d1.getTime();
 
 			long diffSeconds = diff / 1000 % 60;
