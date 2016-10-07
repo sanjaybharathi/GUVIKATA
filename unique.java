0a611f6fd3c2f0ea5bb019df9d4970411880a7b6
sanjaybharathi <sanjaybharathi95@gmail.com>
2016-10-07 14:11:07 +0530

    Create unique.java

---
+++ unique.java
@@ -0,0 +1,21 @@
+public class unique {
+    public static void main(String ar[])throws Exception
+{
+int n,p;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+int ar[]=new int[n];
+for(int i=0;i<n;i++)
+ar[i]=sc.nextInt();
+for(int i=0;i<n;i++)
+{
+p=ar[i];
+for(int j=i+1;j<n;j++)
+{
+if(p==ar[j])
+System.out.println("first repeated integer is:"+p);
+break;
+}
+}
+}
+}
